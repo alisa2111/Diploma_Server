799f2463237fdc049968daa1a93a51400918dfdc
alisa2111 <31954373+alisa2111@users.noreply.github.com>
2019-02-16 15:40:14 +0300

    "Get Account" endpoint

--- src/api/account/index.js
+++ src/api/account/index.js
@@ -1,7 +1,7 @@
 import {middleware as body} from "bodymen";
 import { schema } from './model'
 import { Router } from 'express'
-import {createAccount} from "./controller";
+import {createAccount, getAccount} from "./controller";
 export Account, { schema } from './model'
 
 
@@ -14,5 +14,11 @@ const { balance, owner } = schema.tree;
 router.post('/',
   body({ balance, owner }),
   createAccount);
+
+/**
+ * @api {get} /accounts/:id Get Account
+ */
+router.get('/:id',
+  getAccount);
 
 export default router;
